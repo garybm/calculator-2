888f3a8044b989527e307536489142b42e1ba700
Justin Haupt and Sanyu Prado <sanyu@>
2019-01-14 13:59:15 -0800

    create user interface calculator buttons

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,33 @@ var add = function(number1, number2) {
   return number1 + number2;
 };
 
+var subtract = function(number1, number2) {
+  return number1 - number2;
+};
+
+var multiply = function(number1, number2) {
+  return number1 * number2;
+};
+
+var divide = function(number1, number2) {
+  return number1 / number2;
+};
+
+var bmi = function (height, weight) {
+  return (weight / (height**2)) * 703;
+}
+
+/*
 var number1 = parseInt(prompt("Enter a number:"));
 var number2 = parseInt(prompt("Enter another number:"));
-var result = add(number1, number2);
-alert(result);
+var result1 = add(number1, number2);
+var result2 = subtract(number1, number2);
+var result3 = multiply(number1, number2);
+var result4 = divide(number1, number2);
+var result5 = bmi(number1, number2);
+alert(result1);
+alert(result2);
+alert(result3);
+alert(result4);
+alert(result5);
+*/
