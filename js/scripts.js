9db77b38c3195a7ff211e4a5a73149dbeb1bc517
Gary Bermudez and Jaime Takikawa <gary@>
2019-01-16 14:36:53 -0800

    calculator edditions function

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,6 @@
+
+// Business (or back-end) logic:
+
 var add = function(number1, number2) {
   return number1 + number2;
 };
@@ -14,78 +17,12 @@ var divide = function(number1, number2) {
   return number1 / number2;
 };
 
-var bmi = function (height, weight) {
-  return (weight / (height**2)) * 703;
-}
-
-/*
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
-var result1 = add(number1, number2);
-var result2 = subtract(number1, number2);
-var result3 = multiply(number1, number2);
-var result4 = divide(number1, number2);
-var result5 = bmi(number1, number2);
-alert(result1);
-alert(result2);
-alert(result3);
-alert(result4);
-alert(result5);
-*/
-
-$(function(){
-
-  $("td").click(function() {
-    $('#display').text($('#display').text() + $(this).text());
-
-    if ( $(this).text() === 'C') {
-      $('#display').text('');
-    }
-
-    if ( $(this).text() === '=') {
-      let text = $('#display').text()
-      let length = text.length;
-
-      let op = '';     //operator
-      let left = '';   //left number
-      let right = '';  //right number
-
-      // Identify the operator
-      for(let i=0; i<length; ++i) {
-        if(text.charAt(i)==='*' || text.charAt(i)==='/' || text.charAt(i)==='+' || text.charAt(i)==='-') {
-          op = text.charAt(i);
-          left = text.slice(0, i);
-          right = text.slice(i+1);
-        }
-      }
-
-      left = parseInt(left);
-      right = parseInt(right);
-
-      console.log(op);
-      console.log(left);
-      console.log(right);
-
-      switch(op) {
-        case "+":
-          $('#display').text(left+right);
-          break;
-        case "-":
-          $('#display').text(left-right);
-          break;
-        case "*":
-          $('#display').text(left*right);
-          break;
-
-        case "/":
-          $('#display').text(left/right);
-          break;
-
-        }
-
-
-    }
-
+$(document).ready(function() {
+  $("form#add").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#add1").val());
+    var number2 = parseInt($("#add2").val());
+    var result =add(number1, number2);
+    $("#output").text(result);
   });
-
 });
