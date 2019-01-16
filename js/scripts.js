ff317a46310e1cafed2735058ddb95681ab061f5
Gary Bermudez and Jaime Takikawa <gary@>
2019-01-16 15:07:46 -0800

    add subtract multiply

--- js/scripts.js
+++ js/scripts.js
@@ -23,6 +23,22 @@ $(document).ready(function() {
     var number1 = parseInt($("#add1").val());
     var number2 = parseInt($("#add2").val());
     var result =add(number1, number2);
-    $("#output").text(result);
+    $("#outputadd").text(result);
+  });
+
+  $("form#subtract").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#sub1").val());
+    var number2 = parseInt($("#sub2").val());
+    var result =subtract(number1, number2);
+    $("#outputsub").text(result);
+  });
+
+  $("form#multiply").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#mult1").val());
+    var number2 = parseInt($("#mult2").val());
+    var result =multiply(number1, number2);
+    $("#outputmult").text(result);
   });
 });
